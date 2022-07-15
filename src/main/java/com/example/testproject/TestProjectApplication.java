19eb3a1b357569cc69536aec345532b5a2433bec
Leo <leo.hj.li@aslcn.com.cn>
2022-07-15 15:47:02 +0800

    1

--- src/main/java/com/example/testproject/TestProjectApplication.java
+++ src/main/java/com/example/testproject/TestProjectApplication.java
@@ -2,9 +2,15 @@ package com.example.testproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestProjectApplication {
+public class TestProjectApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestProjectApplication.class, args);
